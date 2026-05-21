ab88b9d0e30223d693799800f5956076422446fd
devamshah16 <devamshah.work@gmail.com>
2026-05-21 17:43:48 +0530

    Update 03_conversionOperation.js

--- JS-Codes/03_conversionOperation.js
+++ JS-Codes/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "hitesh"
+let score = "devam"
 
 //console.log(typeof score);
 //console.log(typeof(score));
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "devam"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
@@ -63,6 +63,3 @@ num1 = num2 = num3 = 2 + 2
 let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
-
-// link to study
-// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
