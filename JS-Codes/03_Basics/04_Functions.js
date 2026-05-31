bd07f01aebb5e66fe500e6c278f95894c95b8aae
devamshah16 <devamshah.work@gmail.com>
2026-05-31 16:39:54 +0530

    Create 04_Functions.js

---
+++ JS-Codes/03_Basics/04_Functions.js
@@ -0,0 +1,8 @@
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject({
+    username: "sam",
+    price: 399
+})
