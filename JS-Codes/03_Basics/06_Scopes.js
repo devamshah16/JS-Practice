8e87d53a85a045bb47831e75f6bb93a353992701
devamshah16 <devamshah.work@gmail.com>
2026-06-01 16:05:06 +0530

    Update and rename 06_Scopes to 06_Scopes.js

---
+++ JS-Codes/03_Basics/06_Scopes.js
@@ -0,0 +1,14 @@
+var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);//here ouput will be INNER:10
+    
+}
+
+
+
+console.log(a);//here the output will 300
+//console.log(b); here b can't be access outside the scope
+console.log(c);//Output:300
