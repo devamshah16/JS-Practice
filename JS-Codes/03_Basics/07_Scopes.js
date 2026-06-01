84dc8d5b0b2b48ee36946fbab7db8564fbd13cc2
devamshah16 <devamshah.work@gmail.com>
2026-06-01 16:08:53 +0530

    Create 07_Scopes.js

---
+++ JS-Codes/03_Basics/07_Scopes.js
@@ -0,0 +1,27 @@
+function one(){
+    const username = "devam"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+     //console.log(website);
+
+     two()
+
+}
+
+ one()
+
+if (true) {
+    const username = "devam"
+    if (username === "devam") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+   //console.log(website);
+}
+
+ //console.log(username);
+
+
