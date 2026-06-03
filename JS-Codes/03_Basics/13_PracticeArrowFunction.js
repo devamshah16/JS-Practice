1936bd66e053da4c70f3d2e53e0ed8969af502b6
devamshah16 <devamshah.work@gmail.com>
2026-06-03 21:41:19 +0530

    Create 13_PracticeArrowFunction.js

---
+++ JS-Codes/03_Basics/13_PracticeArrowFunction.js
@@ -0,0 +1,7 @@
+const addTwo = (num1, num2) => {
+     return num1 + num2
+ }
+console.log(addTwo(3, 4))
+//different way to write the arrow function
+// const addTwo = (num1, num2) =>  num1 + num2 
+// const addTwo = (num1, num2) => ( num1 + num2 )
