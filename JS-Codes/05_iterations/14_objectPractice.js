95e9bb6d637bdc3d20504887e2fc547ab196adc9
devamshah16 <devamshah.work@gmail.com>
2026-07-13 16:55:54 +0530

    Create 14_objectPractice.js

---
+++ JS-Codes/05_iterations/14_objectPractice.js
@@ -0,0 +1,11 @@
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
